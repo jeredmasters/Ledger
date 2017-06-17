79b24c3f714d01e03994ce2367b09b538f30c729
Jered Masters <jered.masters@outlook.com>
2017-06-17 15:51:51 +0800

    built booking editor;

--- database/migrations/2017_06_15_142603_create_bookings_table.php
+++ database/migrations/2017_06_15_142603_create_bookings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatBookingsTable extends Migration
+class CreateBookingsTable extends Migration
 {
     /**
      * Run the migrations.
